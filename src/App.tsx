a6b5f260efff50b20b801e4d536ef55fe6e1a423
keita-makino <kemakino@ucdavis.edu>
2019-09-13 15:05:03 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import Document from '!babel-loader!mdx-loader!./document.mdx';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Document />
     </div>
   );
-}
+};
 
 export default App;
